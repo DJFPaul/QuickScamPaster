dc78932e5d2d8cfcf51a0ca77a3c700d857da9a6
FPaul <35642602+DJFPaul@users.noreply.github.com>
2024-06-04 13:21:29 +0200

    Initial commit

---
+++ QuickScamPaster/Form1.Designer.vb
@@ -0,0 +1,172 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ColorDecayTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Keyobserver = New System.Windows.Forms.Timer(Me.components)
+        Me.Clipboardobserver = New System.Windows.Forms.Timer(Me.components)
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.MinTextBox = New System.Windows.Forms.TextBox()
+        Me.MaxTextBox = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.LightGray
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(126, 31)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Add from clippboard"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.LightGray
+        Me.Button2.Location = New System.Drawing.Point(144, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(126, 31)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Load to clippboard"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox1.Location = New System.Drawing.Point(12, 89)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(258, 369)
+        Me.RichTextBox1.TabIndex = 2
+        Me.RichTextBox1.Text = ""
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 45)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(257, 20)
+        Me.TextBox1.TabIndex = 3
+        Me.TextBox1.Text = "?scam <id>"
+        '
+        'ColorDecayTimer
+        '
+        Me.ColorDecayTimer.Interval = 250
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(200, 69)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(75, 17)
+        Me.CheckBox1.TabIndex = 4
+        Me.CheckBox1.Text = "AutoPaste"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Keyobserver
+        '
+        Me.Keyobserver.Interval = 10
+        '
+        'Clipboardobserver
+        '
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(12, 69)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(67, 17)
+        Me.CheckBox2.TabIndex = 5
+        Me.CheckBox2.Text = "AutoAdd"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(107, 70)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "< Lenght >"
+        '
+        'MinTextBox
+        '
+        Me.MinTextBox.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MinTextBox.Location = New System.Drawing.Point(89, 67)
+        Me.MinTextBox.Name = "MinTextBox"
+        Me.MinTextBox.Size = New System.Drawing.Size(18, 20)
+        Me.MinTextBox.TabIndex = 7
+        Me.MinTextBox.Text = "17"
+        '
+        'MaxTextBox
+        '
+        Me.MaxTextBox.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MaxTextBox.Location = New System.Drawing.Point(165, 67)
+        Me.MaxTextBox.Name = "MaxTextBox"
+        Me.MaxTextBox.Size = New System.Drawing.Size(18, 20)
+        Me.MaxTextBox.TabIndex = 8
+        Me.MaxTextBox.Text = "19"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(281, 469)
+        Me.Controls.Add(Me.MaxTextBox)
+        Me.Controls.Add(Me.MinTextBox)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Quick Scam Paster"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ColorDecayTimer As Timer
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Keyobserver As Timer
+    Friend WithEvents Clipboardobserver As Timer
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents MinTextBox As TextBox
+    Friend WithEvents MaxTextBox As TextBox
+End Class
---
+++ QuickScamPaster/Form1.vb
@@ -0,0 +1,94 @@
+﻿Imports System.Text.RegularExpressions
+Public Class Form1
+    Public Declare Function GetAsyncKeyState Lib "user32" (ByVal vKey As Integer) As Int16
+
+    Dim LastID As String = ""
+    Dim EnterLock As Boolean = False
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If RichTextBox1.Text.Contains(My.Computer.Clipboard.GetText) = True Then
+            Button1.BackColor = Color.Red
+            ColorDecayTimer.Start()
+        Else
+            Button1.BackColor = Color.Green
+            ColorDecayTimer.Start()
+            If RichTextBox1.Text = "" Then
+                RichTextBox1.Text = My.Computer.Clipboard.GetText
+            Else
+                RichTextBox1.Text = RichTextBox1.Text & vbNewLine & My.Computer.Clipboard.GetText
+            End If
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If RichTextBox1.Lines.Count >= 0 And RichTextBox1.Text <> "" Then
+            My.Computer.Clipboard.SetText(TextBox1.Text.Replace("<id>", RichTextBox1.Lines(0)))
+            RichTextBox1.SelectionStart = RichTextBox1.GetFirstCharIndexFromLine(0)
+            RichTextBox1.SelectionLength = RichTextBox1.Lines(0).Length + 1
+            RichTextBox1.SelectedText = String.Empty
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles ColorDecayTimer.Tick
+        ColorDecayTimer.Stop()
+        Button1.BackColor = Color.LightGray
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked = True Then
+            Keyobserver.Start()
+            CheckBox2.CheckState = False
+        Else
+            Keyobserver.Stop()
+        End If
+    End Sub
+
+    Private Sub Keyobserver_Tick(sender As Object, e As EventArgs) Handles Keyobserver.Tick
+
+        If GetAsyncKeyState(Keys.Enter) = -32767 And EnterLock = False Then
+            EnterLock = True
+            System.Threading.Thread.Sleep(50)
+            If RichTextBox1.Lines.Count >= 0 And RichTextBox1.Text <> "" Then
+                SendKeys.SendWait(TextBox1.Text.Replace("<id>", RichTextBox1.Lines(0)))
+                RichTextBox1.SelectionStart = RichTextBox1.GetFirstCharIndexFromLine(0)
+                RichTextBox1.SelectionLength = RichTextBox1.Lines(0).Length + 1
+                RichTextBox1.SelectedText = String.Empty
+            End If
+
+            System.Threading.Thread.Sleep(100)
+            EnterLock = False
+        End If
+    End Sub
+    Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+        If CheckBox2.Checked = True Then
+            LastID = My.Computer.Clipboard.GetText
+            Clipboardobserver.Start()
+            CheckBox1.CheckState = False
+        Else
+            Clipboardobserver.Stop()
+        End If
+    End Sub
+
+    Private Sub Clipboardobserver_Tick(sender As Object, e As EventArgs) Handles Clipboardobserver.Tick
+        Dim ClipBoardCache As String = My.Computer.Clipboard.GetText
+        If ClipBoardCache.Length >= MinTextBox.Text And ClipBoardCache.Length <= MaxTextBox.Text Then
+            If Regex.IsMatch(ClipBoardCache, "^[0-9 ]+$") Then
+                If ClipBoardCache = LastID = False Then
+                    If RichTextBox1.Text.Contains(ClipBoardCache) = False Then
+                        Button1.BackColor = Color.Green
+                        ColorDecayTimer.Start()
+                        If RichTextBox1.Text = "" Then
+                            RichTextBox1.Text = ClipBoardCache
+                            LastID = ClipBoardCache
+                        Else
+                            RichTextBox1.Text = RichTextBox1.Text & vbNewLine & ClipBoardCache
+                        End If
+                    End If
+                End If
+            End If
+        End If
+    End Sub
+End Class
