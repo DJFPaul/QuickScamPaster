f3a545f424b0615d1c9418fb97abd133af280cd7
FPaul <35642602+DJFPaul@users.noreply.github.com>
2025-03-06 12:37:43 +0100

    Bugfixes and Optimisations
    Fixed the manual copy to clipboard button.
    Optimised some code structure and added comments.
    
    If the comand line is empty, always default to using just the ID instead of nothing.

--- QuickScamPaster/Form1.vb
+++ QuickScamPaster/Form1.vb
@@ -8,6 +8,7 @@ Public Class Form1
     Dim MouseLock As Boolean = False
     Dim HasPastedSomething As Boolean = False
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'Copy text from clipboard into RichTextBox1 and advance it accordingly.
         If RichTextBox1.Text.Contains(My.Computer.Clipboard.GetText) = True Then
             Button1.BackColor = Color.Red
             ColorDecayTimer.Start()
@@ -23,26 +24,25 @@ Public Class Form1
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        If RichTextBox1.Lines.Count >= 0 And RichTextBox1.Text <> "" Then
-            My.Computer.Clipboard.SetText(TextBox1.Text.Replace("<id>", RichTextBox1.Lines(0)))
-            RichTextBox1.SelectionStart = RichTextBox1.GetFirstCharIndexFromLine(0)
-            RichTextBox1.SelectionLength = RichTextBox1.Lines(0).Length + 1
-            RichTextBox1.SelectedText = String.Empty
-        End If
+        'Call PasteNextComand with disabled send mode.
+        PasteNextCommand(False)
     End Sub
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles ColorDecayTimer.Tick
+        'This get's started when the button color was changed to revert back to default.
         ColorDecayTimer.Stop()
         Button1.BackColor = Color.LightGray
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Check if command.txt existst and load it's contents into the command field.
         If My.Computer.FileSystem.FileExists(Application.StartupPath & "\command.txt") Then
             TextBox1.Text = My.Computer.FileSystem.ReadAllText(Application.StartupPath & "\command.txt")
         End If
     End Sub
 
     Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        'Checks and starts/stopps key monitoring timer depending on selected mode.
         If CheckBox1.Checked = True Then
             Keyobserver.Start()
             CheckBox2.CheckState = False
@@ -55,26 +55,26 @@ Public Class Form1
 
     Private Sub Keyobserver_Tick(sender As Object, e As EventArgs) Handles Keyobserver.Tick
 
+        'If Auto Paste is enabled this waits for ENTER to be pressed to type in the next ID / Command
         If GetAsyncKeyState(Keys.Enter) = -32767 And EnterLock = False Then
             If GetActiveProcess.ProcessName = ComboBox1.Text Or CheckBox4.Checked = False Then
                 EnterLock = True
                 System.Threading.Thread.Sleep(64)
-                If RichTextBox1.Lines.Count >= 0 And RichTextBox1.Text <> "" Then
-                    SendKeys.SendWait(TextBox1.Text.Replace("<id>", RichTextBox1.Lines(0)))
-                    RichTextBox1.SelectionStart = RichTextBox1.GetFirstCharIndexFromLine(0)
-                    RichTextBox1.SelectionLength = RichTextBox1.Lines(0).Length + 1
-                    RichTextBox1.SelectedText = String.Empty
-                End If
-
+                'Call PasteNextComand with enabled send mode.
+                PasteNextCommand(True)
                 System.Threading.Thread.Sleep(64)
                 EnterLock = False
             End If
         End If
 
+        'If Auto+ is enabled, waits for the LMouse button to be pressed.
         If GetAsyncKeyState(Keys.LButton) = -32767 And CheckBox3.Checked = True And MouseLock = False Then
+            'If Appcheck is enabled make sure the current window to paste into is the whitelisted window.
             If GetActiveProcess.ProcessName = ComboBox1.Text Or CheckBox4.Checked = False Then
                 MouseLock = True
                 System.Threading.Thread.Sleep(64)
+
+                'Tap into text box to prepare typing in the command.
                 If RichTextBox1.Lines.Count >= 0 And RichTextBox1.Text <> "" Or HasPastedSomething = True Then
                     SendKeys.Send("{TAB}")
                 End If
@@ -84,36 +84,49 @@ Public Class Form1
                 End While
                 System.Threading.Thread.Sleep(64)
 
+                'If RichTextbox1 is not empty, paste the next command and send it with the ENTER key.
+                If RichTextBox1.Lines.Count >= 0 And RichTextBox1.Text <> "" Then
+                    System.Threading.Thread.Sleep(64)
+                    SendKeys.Send("{ENTER}")
+                    System.Threading.Thread.Sleep(128)
+                    'Call PasteNextComand with enabled send mode.
+                    PasteNextCommand(True)
+                    HasPastedSomething = True
 
-                If True = True Then
-                    If RichTextBox1.Lines.Count >= 0 And RichTextBox1.Text <> "" Then
-                        System.Threading.Thread.Sleep(64)
-                        SendKeys.Send("{ENTER}")
-
-                        System.Threading.Thread.Sleep(128)
-                        If RichTextBox1.Lines.Count >= 0 And RichTextBox1.Text <> "" Then
-                            SendKeys.SendWait(TextBox1.Text.Replace("<id>", RichTextBox1.Lines(0)))
-                            RichTextBox1.SelectionStart = RichTextBox1.GetFirstCharIndexFromLine(0)
-                            RichTextBox1.SelectionLength = RichTextBox1.Lines(0).Length + 1
-                            RichTextBox1.SelectedText = String.Empty
-                        End If
-                        HasPastedSomething = True
-                    ElseIf HasPastedSomething = True Then
-                        HasPastedSomething = False
-                        System.Threading.Thread.Sleep(64)
-                        SendKeys.Send("{ENTER}")
-
-                        System.Threading.Thread.Sleep(128)
-
-                    End If
+                    'If RichTextbox1 has just become empty, send a last ENTER command to send the text that should still be present in the windows chat line.
+                ElseIf HasPastedSomething = True Then
+                    HasPastedSomething = False
+                    System.Threading.Thread.Sleep(64)
+                    SendKeys.Send("{ENTER}")
+                    System.Threading.Thread.Sleep(128)
                 End If
             End If
-
-
             MouseLock = False
         End If
     End Sub
+    Private Function PasteNextCommand(PasteMode As Boolean)
+        'This function handled the loading and pasting or copying of the next command, then removes the current first line from the RichTextbox1 and advances it accordingly
+        If RichTextBox1.Lines.Count >= 0 And RichTextBox1.Text <> "" Then
+            If PasteMode = True Then
+                If TextBox1.Text = "" Then
+                    SendKeys.SendWait(RichTextBox1.Lines(0))
+                Else
+                    SendKeys.SendWait(TextBox1.Text.Replace("<id>", RichTextBox1.Lines(0)))
+                End If
+            Else
+                If TextBox1.Text = "" Then
+                    Clipboard.SetText(RichTextBox1.Lines(0))
+                Else
+                    Clipboard.SetText(TextBox1.Text.Replace("<id>", RichTextBox1.Lines(0)))
+                End If
+            End If
+            RichTextBox1.SelectionStart = RichTextBox1.GetFirstCharIndexFromLine(0)
+            RichTextBox1.SelectionLength = RichTextBox1.Lines(0).Length + 1
+            RichTextBox1.SelectedText = String.Empty
+        End If
+    End Function
     Private Sub CheckBox2_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox2.CheckedChanged
+        'Handles starting or stopping of the clipboard detection function.
         If CheckBox2.Checked = True Then
             'LastID = My.Computer.Clipboard.GetText
             Clipboardobserver.Start()
@@ -124,12 +137,17 @@ Public Class Form1
     End Sub
 
     Private Sub Clipboardobserver_Tick(sender As Object, e As EventArgs) Handles Clipboardobserver.Tick
+        'This function monitors the clipboard for ID's being copied, if AutoADD is enabled and thus this timer is running.
         Dim ClipBoardCache As String = My.Computer.Clipboard.GetText
+        'Check if the lenght is within specified MIN/MAX detection range.
         If ClipBoardCache.Length >= MinTextBox.Text And ClipBoardCache.Length <= MaxTextBox.Text Then
+            'Check that it's only numbers.
             If Regex.IsMatch(ClipBoardCache, "^[0-9 ]+$") Then
+                'Handle adding and give a feedback to the user by the copy button green.
                 If ClipBoardCache = LastID = False Then
                     If RichTextBox1.Text.Contains(ClipBoardCache) = False Then
                         Button1.BackColor = Color.Green
+                        'Start delay timer to make the button color normal again.
                         ColorDecayTimer.Start()
                         If RichTextBox1.Text = "" Then
                             RichTextBox1.Text = ClipBoardCache
@@ -152,6 +170,7 @@ Public Class Form1
     End Function
 
     Private Sub RichTextBox1_TextChanged(sender As Object, e As EventArgs) Handles RichTextBox1.TextChanged
+        'Update line conter label if text changed.
         Label2.Text = RichTextBox1.Lines.Length
     End Sub
 End Class
